318220380768e45e2cabc8a2449f3ba182d6ac9a
tom.hunter <tom.hunter@cityandguilds.com>
2025-02-21 10:04:19 +0000

    Persistence added
    Added a while loop to the execution script to allow to run in the background persistently while I do other jobs

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,5 +1,13 @@
+from datetime import datetime, time
 from def_process_year import process_year
 
+
+def is_before_5pm():
+    now = datetime.now()
+    return now.time() < time(17, 0)
+
+
 if __name__ == "__main__":
-    year = "2016"  # Change to the desired year if needed
-    process_year(year)
+    year = "2018"  # Change to the desired year
+    while is_before_5pm():
+        process_year(year)
